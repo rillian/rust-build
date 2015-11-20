069e44b452f5076cc2be759ea23344d7f3ca5a27
Ralph Giles <giles@mozilla.com>
2015-11-20 13:57:12 -0800

    Try to use all available cores to speed up the build.

--- build.sh
+++ build.sh
@@ -10,12 +10,14 @@ set -x -e
 
 : WORKSPACE                     ${WORKSPACE:=/home/worker/workspace}
 
+CORES=$(nproc || grep -c ^processor /proc/cpuinfo || sysctl -n hw.ncpu)
+
 set -v
 
 # Configure and build rust.
 pushd ${WORKSPACE}/rust
 ./configure --prefix=${WORKSPACE}/rustc --enable-rpath
-make
+make -j ${CORES}
 make install
 popd
 
