a7eb92fc0c3aada934e5f64b0211788685585e1f
Ralph Giles <giles@mozilla.com>
2015-11-23 17:48:15 -0800

    Download gcc release and deps.

--- base-centos6/checkout-sources.sh
+++ base-centos6/checkout-sources.sh
@@ -14,4 +14,44 @@ set -x -e
 set -v
 
 # Check out rust sources
-git clone $REPOSITORY -b $BRANCH ${WORKSPACE}/gcc
+#git clone $REPOSITORY -b $BRANCH ${WORKSPACE}/gcc
+
+# Download release
+VERSION=5.2.0
+mkdir -p ${WORKSPACE}
+pushd ${WORKSPACE}
+# gcc
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 0xFC26A641
+curl -Os http://ftp.gnu.org/gnu/gcc/gcc-${VERSION}/gcc-${VERSION}.tar.bz2.sig
+curl -Os http://gcc.skazkaforyou.com/releases/gcc-${VERSION}/gcc-${VERSION}.tar.bz2
+gpg --verify gcc-${VERSION}.tar.bz2.sig
+tar xf gcc-${VERSION}.tar.bz2
+
+# deps
+
+curl -Os ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-0.8.1.tar.gz
+curl -Os ftp://gcc.gnu.org/pub/gcc/infrastructure/mpfr-2.4.2.tar.bz2
+curl -Os ftp://gcc.gnu.org/pub/gcc/infrastructure/gmp-4.3.2.tar.bz2
+
+GMP_VERSION=6.1.0
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 0xF3599FF828C67298
+curl -Os https://gmplib.org/download/gmp/gmp-${GMP_VERSION}.tar.xz.sig
+curl -Os https://gmplib.org/download/gmp/gmp-${GMP_VERSION}.tar.xz
+gpg --verify gmp-${GMP_VERSION}.tar.xz.sig
+tar xf gmp-${GMP_VERSION}.tar.xz
+
+MPFR_VERSION=3.1.3
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 0x980C197698C3739D
+curl -Os http://www.mpfr.org/mpfr-current/mpfr-${MPFR_VERSION}.tar.xz.asc
+curl -Os http://www.mpfr.org/mpfr-current/mpfr-${MPFR_VERSION}.tar.xz
+curl -Os https://gmplib.org/download/gmp/gmp-${MPFR_VERSION}.tar.xz
+gpg --verify mpfr-${MPFR_VERSION}.tar.xz.asc
+tar xf mpfr-${MPFR_VERSION}.tar.xz
+
+MPC_VERSION=1.0.3
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 0xF7D5C9BF765C61E3
+curl -Os ftp://ftp.gnu.org/gnu/mpc/mpc-${MPC_VERSION}.tar.gz.sig
+curl -Os ftp://ftp.gnu.org/gnu/mpc/mpc-${MPC_VERSION}.tar.gz
+gpg --verify mpc-${MPC_VERSION}.tar.gz.sig
+tar xf mpc-${MPC_VERSION}.tar.gz
+popd
