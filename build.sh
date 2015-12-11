d2f1c811b549e1e566c72e02fb6ad978843bbcda
Ralph Giles <giles@mozilla.com>
2015-12-11 00:00:59 -0500

    Build with support for the 32 bit cross target.
    Build both 32 and 64 bit versions and copy the standard library
    from the 32 bit build into the 64 bit install image. This is
    wasteful, but I don't know how to build just the standard library.

--- build.sh
+++ build.sh
@@ -9,14 +9,29 @@ CORES=$(nproc || grep -c ^processor /proc/cpuinfo || sysctl -n hw.ncpu)
 set -v
 
 # Configure and build rust.
-pushd ${WORKSPACE}/rust
-./configure --prefix=${WORKSPACE}/rustc --enable-rpath --disable-docs
+OPTIONS="--enable-rpath --disable-docs"
+M32="i686-unknown-linux-gnu"
+M64="x86_64-unknown-linux-gnu"
+
+mkdir -p ${WORKSPACE}/rust-build
+pushd ${WORKSPACE}/rust-build
+mkdir -p ${M32}
+pushd ${M32}
+${WORKSPACE}/rust/configure --prefix=${PWD}/../rustc --build=${M32} ${OPTIONS}
+make -j ${CORES}
+popd
+mkdir -p ${M64}
+pushd ${M64}
+${WORKSPACE}/rust/configure --prefix=${PWD}/../rustc --build=${M64} ${OPTIONS}
 make -j ${CORES}
 make install
 popd
+# Copy 32 bit stdlib into the install directory for cross support.
+cp -r ${M32}/${M32}/lib/rustlib/${M32} rustc/lib/rustlib/
+popd
 
 # Package the toolchain for upload.
-pushd ${WORKSPACE}
+pushd ${WORKSPACE}/rust-build
 tar cvJf rustc.tar.xz rustc/*
 /builds/tooltool.py add --visibility=public rustc.tar.xz
 popd
