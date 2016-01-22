77f57d9264188241f25fccef162b08ad5bac6090
Ralph Giles <giles@mozilla.com>
2016-01-21 19:48:33 -0800

    Add draft mac build script.

---
+++ build_rust_mac.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -vex
+
+: WORKSPACE ${WORKSPACE:=$PWD}
+
+CORES=$(nproc || grep -c ^processor /proc/cpuinfo || sysctl -n hw.ncpu)
+echo Building on $CORES cpus...
+
+OPTIONS="--enable-rpath --disable-elf-tls --disable-docs"
+TARGETS="x86_64-apple-darwin,i686-apple-darwin"
+
+PREFIX=${WORKSPACE}/rustc
+
+mkdir -p ${WORKSPACE}/gecko-rust-mac
+pushd ${WORKSPACE}/gecko-rust-mac
+
+${WORKSPACE}/rust/configure --prefix=${PREFIX} --target=${TARGETS} ${OPTIONS}
+make -j ${CORES}
+
+rm -rf ${PREFIX}
+mkdir ${PREFIX}
+make install
+popd
+
+# Package the toolchain for upload.
+pushd ${WORKSPACE}
+tar cvjf rustc.tar.bz2 rustc/*
+python tooltool.py add --visibility=public --unpack rustc.tar.bz2
+popd
