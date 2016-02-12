dd02a28c75406d7b18ee17cbf43cc13d9ce9c803
Ralph Giles <giles@mozilla.com>
2016-02-12 15:50:58 -0800

    Record rust version and add it to the tooltool upload message.
    Also fix some typos in upload_rust.sh.

--- upload_rust.sh
+++ upload_rust.sh
@@ -10,12 +10,15 @@ set -v
 
 # Upload artifacts packaged by the build script.
 pushd ${WORKSPACE}
-if test -n "$TASK_ID";
+if test -n "$TASK_ID"; then
   # If we're running on task cluster, use the upload-capable tunnel.
   TOOLTOOL_OPTS="--url=http://relengapi/tooltool/"
   MESSAGE="Taskcluster upload ${TASK_ID}/${RUN_ID} $0"
 else
   MESSAGE="Rust toolchain build for gecko"
 fi
-/build/tooltool.py upload ${TOOLTOOL_OPTS} --msg=${MESSAGE}
+if test -r rust-version; then
+  MESSAGE="$MESSAGE $(cat rust-version)"
+fi
+/build/tooltool.py upload ${TOOLTOOL_OPTS} --message="${MESSAGE}"
 popd
