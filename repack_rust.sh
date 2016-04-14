c40fed90280058797bf39109bbc09cf002caf358
Ralph Giles <giles@mozilla.com>
2016-04-14 12:39:30 -0700

    Generalize the fetch and repack code.
    This constructs repacks for linux64, win64 and win32.

--- repack_rust.sh
+++ repack_rust.sh
@@ -25,11 +25,72 @@ verify() {
   keybase verify ${1}.asc
 }
 
+fetch_rustc() {
+  arch=$1
+  echo "Retrieving rustc build for $arch..."
+  for pkg in $(cat ${IDX} | grep ^rustc | grep $arch); do
+    fetch ${pkg}
+    verify ${pkg}
+  done
+}
+
+fetch_std() {
+  echo "Retrieving rust-std builds for $@"
+  for arch in $@; do
+    for pkg in $(cat ${IDX} | grep rust-std | grep $arch); do
+      fetch ${pkg}
+      verify ${pkg}
+    done
+  done
+}
+
+install_rustc() {
+  pkg=$(cat ${IDX} | grep ^rustc | grep $1)
+  base=${pkg%%.tar.*}
+  echo "Installing $base..."
+  tar xf ${pkg}
+  ${base}/install.sh ${INSTALL_OPTS}
+  rm -rf ${base}
+}
+
+install_std() {
+  for arch in $@; do
+    for pkg in $(cat ${IDX} | grep rust-std | grep $arch); do
+      base=${pkg%%.tar.*}
+      echo "Installing $base..."
+      tar xf ${pkg}
+      ${base}/install.sh ${INSTALL_OPTS}
+      rm -rf ${base}
+    done
+  done
+}
+
+check() {
+  if test -x ${TARGET}/bin/rustc; then
+    ${TARGET}/bin/rustc --version
+  elif test -x ${TARGET}/bin/rustc.exe; then
+    ${TARGET}/bin/rustc.exe --version
+  else
+    echo "ERROR: Couldn't fine rustc executable"
+    exit 1
+  fi
+  echo "Installed components:"
+  for component in $(cat ${TARGET}/lib/rustlib/components); do
+    echo "  $component"
+  done
+  echo
+}
+
 set -v
 
-x32="i686-unknown-linux-gnu"
-x64="x86_64-unknown-linux-gnu"
-arm="arm-linux-androideabi"
+linux64="x86_64-unknown-linux-gnu"
+linux32="i686-unknown-linux-gnu"
+
+android="arm-linux-androideabi"
+
+win64="x86_64-pc-windows-msvc"
+win32="i686-pc-windows-msvc"
+win32_i586="i586-pc-windows-msvc"
 
 # Fetch the manifest
 
@@ -38,44 +99,46 @@ IDX=channel-rustc-${RUST_CHANNEL}
 fetch ${IDX}
 verify ${IDX}
 
-for arch in $x32 $x64; do
-  for pkg in $(cat ${IDX} | grep ^rustc | grep $arch); do
-    fetch ${pkg}
-    verify ${pkg}
-  done
-  for pkg in $(cat ${IDX} | grep rust-std | grep $arch); do
-    fetch ${pkg}
-    verify ${pkg}
-  done
-done
-  
-# Unpack the bits we want.
-
 TARGET=rustc
 INSTALL_OPTS="--prefix=${PWD}/${TARGET} --disable-ldconfig"
+
+# Repack the linux64 builds.
+
+fetch_rustc $linux64
+fetch_std $linux64 $linux32
+
 rm -rf ${TARGET}
 
-# Install rustc.
-pkg=$(cat ${IDX} | grep ^rustc | grep $x64)
-base=${pkg%%.tar.*}
-tar xf ${pkg}
-${base}/install.sh ${INSTALL_OPTS}
-rm -rf ${base}
+install_rustc $linux64
+install_std $linux64 $linux32
 
-# Install standard libraries for targets we need.
-for arch in $x32 $x64; do
-  for pkg in $(cat ${IDX} | grep rust-std | grep $arch); do
-    base=${pkg%%.tar.*}
-    tar xf ${pkg}
-    ${base}/install.sh ${INSTALL_OPTS}
-    rm -rf ${base}
-  done
-done
+tar cJf rustc-$linux64-repack.tar.xz ${TARGET}/*
+check ${TARGET}
 
-${TARGET}/bin/rustc --version
-echo "Installed components:"
-cat ${TARGET}/lib/rustlib/components
-echo
+# Repack the win64 builds.
 
-# Repack for distribution.
-tar cvJf rustc.tar.xz ${TARGET}/*
+fetch_rustc $win64
+fetch_std $win64
+
+rm -rf ${TARGET}
+
+install_rustc $win64
+install_std $win64
+
+tar cjf rustc-$win64-repack.tar.bz2 ${TARGET}/*
+check ${TARGET}
+
+# Repack the win32 builds.
+
+fetch_rustc $win32
+fetch_std $win32 $win32_i586
+
+rm -rf ${TARGET}
+
+install_rustc $win32
+install_std $win32 $win32_i586
+
+tar cjf rustc-$win32-repack.tar.bz2 ${TARGET}/*
+check ${TARGET}
+
+rm -rf ${TARGET}
