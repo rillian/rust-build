6e5c8dbfc2dfbdd4bb0290c92c9e8e634992ae1d
Ralph Giles <giles@mozilla.com>
2016-07-13 14:37:21 -0700

    Port repack script to python.
    We want to include cargo in our repacks, but the manifest linking
    cargo to a particular rust release seems to only be available
    in toml, which is hard to parse in shell.
    
    An alternative would be to go back to using the channel-rust-stable
    manifest and adding targets from channel-rustc-stable, but this
    seems less brittle, and I believe this is the resource supporting rustup.

--- fetch_cargo.sh
+++ fetch_cargo.sh
@@ -4,17 +4,18 @@ set -x -e
 
 # Inputs, with defaults
 
-: RUST_REPOSITORY ${RUST_REPOSITORY:=https://github.com/rust-lang/cargo}
-: RUST_BRANCH     ${RUST_BRANCH:=master}
+: REPOSITORY   ${REPOSITORY:=https://github.com/rust-lang/cargo}
+: BRANCH       ${BRANCH:=master}
 
-: WORKSPACE       ${WORKSPACE:=/home/worker}
+: WORKSPACE    ${WORKSPACE:=/home/worker}
 
 set -v
 
 # Check out rust sources
-git clone --recursive $RUST_REPOSITORY -b $RUST_BRANCH ${WORKSPACE}/cargo
+SRCDIR=${WORKSPACE}/cargo
+git clone --recursive $REPOSITORY -b $BRANCH ${SRCDIR}
 
 # Report version
-VERSION=$(git -C ${WORKSPACE}/cargo describe --tags --dirty)
-COMMIT=$(git -C ${WORKSPACE}/cargo rev-parse HEAD)
+VERSION=$(git -C ${SRCDIR} describe --tags --dirty)
+COMMIT=$(git -C ${SRCDIR} rev-parse HEAD)
 echo "cargo ${VERSION} (commit ${COMMIT})" | tee cargo-version
---
+++ repack_rust.py
@@ -0,0 +1,242 @@
+#!/bin/env python
+'''
+This script downloads and repacks official rust language builds
+with the necessary tool and target support for the Firefox
+build environment.
+'''
+
+import requests
+import toml
+import os
+
+def fetch_file(url):
+  '''Download a file from the given url if it's not already present.'''
+  filename = os.path.basename(url)
+  if not os.path.exists(filename):
+    os.system('curl -Os %s' %url)
+
+def fetch(url):
+  '''Download and verify a package url.'''
+  print('Fetching %s...' % url)
+  fetch_file('curl -Os %s.asc' % url)
+  fetch_file('curl -Os %s' % url)
+  fetch_file('curl -Os %s.sha256' % url)
+  fetch_file('curl -Os %s.asc.sha256' % url)
+  base = os.path.basename(url)
+  print('Verifying %s...' % base)
+  # TODO: check for verification failure.
+  os.system('shasum -c %s.sha256' % base)
+  os.system('shasum -c %s.asc.sha256' % base)
+  os.system('gpg --verify %s.asc %s' % (base, base))
+  os.system('keybase verify %s.asc' % base)
+
+def install(filename, target):
+  '''Run a package's installer script against the given target directory.'''
+  print(' Unpacking %s...' % filename)
+  os.system('tar xf ' + filename)
+  basename = filename.split('.tar')[0]
+  print(' Installing %s...' % basename)
+  install_opts = '--prefix=${PWD}/%s --disable-ldconfig' % target
+  os.system('%s/install.sh %s' % (basename, install_opts))
+  print(' Cleaning %s...' % basename)
+  os.system('rm -rf %s' % basename)
+
+def package(manifest, pkg, target):
+  '''Pull out the package dict for a particular package and target
+  from the given manifest.'''
+  version = manifest['pkg'][pkg]['version']
+  info = manifest['pkg'][pkg]['target'][target]
+  return (version, info)
+
+def repack(host, targets, channel='stable'):
+  url = 'https://static.rust-lang.org/dist/channel-rust-' + channel + '.toml'
+  req = requests.get(url)
+  req.raise_for_status()
+  manifest = toml.loads(req.content)
+  if manifest['manifest-version'] != '2':
+    print('ERROR: unrecognized manifest version %s.' % manifest['manifest-version'])
+    return
+  print('Using manifest for rust %s as of %s.' % (channel, manifest['date']))
+  rustc_version, rustc = package(manifest, 'rustc', host)
+  if rustc['available']:
+    print('rustc %s\n  %s\n  %s' % (rustc_version, rustc['url'], rustc['hash']))
+    fetch(rustc['url'])
+  cargo_version, cargo = package(manifest, 'cargo', host)
+  if cargo['available']:
+    print('cargo %s\n  %s\n  %s' % (cargo_version, cargo['url'], cargo['hash']))
+    fetch(cargo['url'])
+  stds = []
+  for target in targets:
+      version, info = package(manifest, 'rust-std', target)
+      if info['available']:
+        print('rust-std %s\n  %s\n  %s' % (version, info['url'], info['hash']))
+        fetch(info['url'])
+        stds.append(info)
+  print('Installing packages...')
+  tar_basename = 'rustc-%s-repack' % host
+  install_dir = 'rustc'
+  os.system('rm -rf %s' % install_dir)
+  install(os.path.basename(rustc['url']), install_dir)
+  install(os.path.basename(cargo['url']), install_dir)
+  for std in stds:
+    install(os.path.basename(std['url']), install_dir)
+  print('Tarring %s...' % tar_basename)
+  os.system('tar cjf %s.tar.bz2 %s/*' % (tar_basename, install_dir))
+  os.system('rm -rf %s' % install_dir)
+
+# rust platform triples
+android="arm-linux-androideabi"
+linux64="x86_64-unknown-linux-gnu"
+linux32="i686-unknown-linux-gnu"
+mac64="x86_64-apple-darwin"
+mac32="i686-apple-darwin"
+win64="x86_64-pc-windows-msvc"
+win32="i686-pc-windows-msvc"
+
+if __name__ == '__main__':
+  repack(mac64, [mac64, mac32])
+  repack(win32, [win32])
+  repack(win64, [win64])
+  repack(linux64, [linux64, linux32])
+
+'''
+install_rustc() {
+  pkg=$(cat ${IDX} | grep ^rustc | grep $1)
+  base=${pkg%%.tar.*}
+  echo "Installing $base..."
+  tar xf ${pkg}
+  ${base}/install.sh ${INSTALL_OPTS}
+  rm -rf ${base}
+}
+
+install_std() {
+  for arch in $@; do
+    for pkg in $(cat ${IDX} | grep rust-std | grep $arch); do
+      base=${pkg%%.tar.*}
+      echo "Installing $base..."
+      tar xf ${pkg}
+      ${base}/install.sh ${INSTALL_OPTS}
+      rm -rf ${base}
+    done
+  done
+}
+
+check() {
+  if test -x ${TARGET}/bin/rustc; then
+    file ${TARGET}/bin/rustc
+    ${TARGET}/bin/rustc --version
+  elif test -x ${TARGET}/bin/rustc.exe; then
+    file ${TARGET}/bin/rustc.exe
+    ${TARGET}/bin/rustc.exe --version
+  else
+    die "ERROR: Couldn't fine rustc executable"
+  fi
+  echo "Installed components:"
+  for component in $(cat ${TARGET}/lib/rustlib/components); do
+    echo "  $component"
+  done
+  echo
+}
+
+test -n "$TASK_ID" && set -v
+
+linux64="x86_64-unknown-linux-gnu"
+linux32="i686-unknown-linux-gnu"
+
+android="arm-linux-androideabi"
+
+mac64="x86_64-apple-darwin"
+mac32="i686-apple-darwin"
+
+win64="x86_64-pc-windows-msvc"
+win32="i686-pc-windows-msvc"
+win32_i586="i586-pc-windows-msvc"
+
+# Fetch the manifest
+
+IDX=channel-rustc-${RUST_CHANNEL}
+
+fetch ${IDX}
+verify ${IDX}
+
+TARGET=rustc
+INSTALL_OPTS="--prefix=${PWD}/${TARGET} --disable-ldconfig"
+
+# Repack the linux64 builds.
+repack_linux64() {
+  fetch_rustc $linux64
+  fetch_std $linux64 $linux32
+
+  rm -rf ${TARGET}
+
+  install_rustc $linux64
+  install_std $linux64 $linux32
+
+  tar cJf rustc-$linux64-repack.tar.xz ${TARGET}/*
+  check ${TARGET}
+}
+
+# Repack the win64 builds.
+repack_win64() {
+  fetch_rustc $win64
+  fetch_std $win64
+
+  rm -rf ${TARGET}
+
+  install_rustc $win64
+  install_std $win64
+
+  tar cjf rustc-$win64-repack.tar.bz2 ${TARGET}/*
+  check ${TARGET}
+}
+
+# Repack the win32 builds.
+repack_win32() {
+  fetch_rustc $win32
+  fetch_std $win32
+
+  rm -rf ${TARGET}
+
+  install_rustc $win32
+  install_std $win32
+
+  tar cjf rustc-$win32-repack.tar.bz2 ${TARGET}/*
+  check ${TARGET}
+}
+
+# Repack the mac builds.
+repack_mac() {
+  fetch_rustc $mac64
+  fetch_std $mac64 $mac32
+
+  rm -rf ${TARGET}
+
+  install_rustc $mac64
+  install_std $mac64 $mac32
+
+  tar cjf rustc-mac-repack.tar.bz2 ${TARGET}/*
+  check ${TARGET}
+}
+
+# Repack mac cross build.
+repack_mac_cross() {
+  fetch_rustc $linux64
+  fetch_std $linux64
+
+  rm -rf ${TARGET}
+
+  install_rustc $linux64
+  install_std $linux64 $mac64 $mac32
+
+  tar cJf rustc-mac-cross-repack.tar.xz ${TARGET}/*
+  check ${TARGET}
+}
+
+repack_win32
+repack_win64
+repack_linux64
+repack_mac
+repack_mac_cross
+
+rm -rf ${TARGET}
+'''
