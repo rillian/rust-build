5a49c42f81228f8937185eae2831e03047d4dab2
Ralph Giles <giles@mozilla.com>
2016-07-19 14:48:22 -0400

    Add linux-hosted mac-cross target repack.
    We use these on taskcluster to build for mac.

--- repack_rust.py
+++ repack_rust.py
@@ -56,7 +56,7 @@ def package(manifest, pkg, target):
   info = manifest['pkg'][pkg]['target'][target]
   return (version, info)
 
-def repack(host, targets, channel='stable'):
+def repack(host, targets, channel='stable', suffix=''):
   print("Repacking rust for %s..." % host)
   url = 'https://static.rust-lang.org/dist/channel-rust-' + channel + '.toml'
   req = requests.get(url)
@@ -82,7 +82,10 @@ def repack(host, targets, channel='stable'):
         fetch(info['url'])
         stds.append(info)
   print('Installing packages...')
-  tar_basename = 'rustc-%s-repack' % host
+  tar_basename = 'rustc-' + host
+  if suffix:
+      tar_basename += '-' + suffix
+  tar_basename += '-repack'
   install_dir = 'rustc'
   subprocess.check_call(['rm', '-rf', install_dir])
   install(os.path.basename(rustc['url']), install_dir)
@@ -107,145 +110,4 @@ if __name__ == '__main__':
   repack(win32, [win32])
   repack(win64, [win64])
   repack(linux64, [linux64, linux32])
-
-'''
-install_rustc() {
-  pkg=$(cat ${IDX} | grep ^rustc | grep $1)
-  base=${pkg%%.tar.*}
-  echo "Installing $base..."
-  tar xf ${pkg}
-  ${base}/install.sh ${INSTALL_OPTS}
-  rm -rf ${base}
-}
-
-install_std() {
-  for arch in $@; do
-    for pkg in $(cat ${IDX} | grep rust-std | grep $arch); do
-      base=${pkg%%.tar.*}
-      echo "Installing $base..."
-      tar xf ${pkg}
-      ${base}/install.sh ${INSTALL_OPTS}
-      rm -rf ${base}
-    done
-  done
-}
-
-check() {
-  if test -x ${TARGET}/bin/rustc; then
-    file ${TARGET}/bin/rustc
-    ${TARGET}/bin/rustc --version
-  elif test -x ${TARGET}/bin/rustc.exe; then
-    file ${TARGET}/bin/rustc.exe
-    ${TARGET}/bin/rustc.exe --version
-  else
-    die "ERROR: Couldn't fine rustc executable"
-  fi
-  echo "Installed components:"
-  for component in $(cat ${TARGET}/lib/rustlib/components); do
-    echo "  $component"
-  done
-  echo
-}
-
-test -n "$TASK_ID" && set -v
-
-linux64="x86_64-unknown-linux-gnu"
-linux32="i686-unknown-linux-gnu"
-
-android="arm-linux-androideabi"
-
-mac64="x86_64-apple-darwin"
-mac32="i686-apple-darwin"
-
-win64="x86_64-pc-windows-msvc"
-win32="i686-pc-windows-msvc"
-win32_i586="i586-pc-windows-msvc"
-
-# Fetch the manifest
-
-IDX=channel-rustc-${RUST_CHANNEL}
-
-fetch ${IDX}
-verify ${IDX}
-
-TARGET=rustc
-INSTALL_OPTS="--prefix=${PWD}/${TARGET} --disable-ldconfig"
-
-# Repack the linux64 builds.
-repack_linux64() {
-  fetch_rustc $linux64
-  fetch_std $linux64 $linux32
-
-  rm -rf ${TARGET}
-
-  install_rustc $linux64
-  install_std $linux64 $linux32
-
-  tar cJf rustc-$linux64-repack.tar.xz ${TARGET}/*
-  check ${TARGET}
-}
-
-# Repack the win64 builds.
-repack_win64() {
-  fetch_rustc $win64
-  fetch_std $win64
-
-  rm -rf ${TARGET}
-
-  install_rustc $win64
-  install_std $win64
-
-  tar cjf rustc-$win64-repack.tar.bz2 ${TARGET}/*
-  check ${TARGET}
-}
-
-# Repack the win32 builds.
-repack_win32() {
-  fetch_rustc $win32
-  fetch_std $win32
-
-  rm -rf ${TARGET}
-
-  install_rustc $win32
-  install_std $win32
-
-  tar cjf rustc-$win32-repack.tar.bz2 ${TARGET}/*
-  check ${TARGET}
-}
-
-# Repack the mac builds.
-repack_mac() {
-  fetch_rustc $mac64
-  fetch_std $mac64 $mac32
-
-  rm -rf ${TARGET}
-
-  install_rustc $mac64
-  install_std $mac64 $mac32
-
-  tar cjf rustc-mac-repack.tar.bz2 ${TARGET}/*
-  check ${TARGET}
-}
-
-# Repack mac cross build.
-repack_mac_cross() {
-  fetch_rustc $linux64
-  fetch_std $linux64
-
-  rm -rf ${TARGET}
-
-  install_rustc $linux64
-  install_std $linux64 $mac64 $mac32
-
-  tar cJf rustc-mac-cross-repack.tar.xz ${TARGET}/*
-  check ${TARGET}
-}
-
-repack_win32
-repack_win64
-repack_linux64
-repack_mac
-repack_mac_cross
-
-rm -rf ${TARGET}
-'''
+  repack(linux64, [linux64, mac64, mac32], suffix='mac-cross')
