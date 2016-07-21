0d16570f3c02c23edf58dac3279133706c278e61
Ralph Giles <giles@mozilla.com>
2016-07-20 21:36:00 -0400

    Set MACOSX_DEPLOYMENT_TARGET when building cargo on darwin.
    This is needed for the build to work on older mac installs.

--- build_cargo.sh
+++ build_cargo.sh
@@ -8,8 +8,13 @@ set -v
 
 # Configure and build cargo.
 
+if test $(uname -s) = "Darwin"; then
+  export MACOSX_DEPLOYMENT_TARGET=10.7
+fi
+
 pushd ${WORKSPACE}/cargo
 ./configure --prefix=${WORKSPACE}/rustc --local-rust-root=${WORKSPACE}/rustc
 make
+make dist
 make install
 popd
