df4375cc3b94862f4115e78eaa50522427d0493b
Ralph Giles <giles@mozilla.com>
2017-01-03 14:52:08 -0800

    Merge formatting changes from the gecko version.
    Keep the flake8 linter happy.

--- logtail.py
+++ logtail.py
@@ -4,12 +4,14 @@ import requests.packages.urllib3
 import os.path
 import sys
 
+
 def fetch_log(task, run):
-  auth = tcbuild.read_tc_auth(os.path.expanduser('~/.tcauth'))
-  queue = taskcluster.Queue({'credentials': auth})
-  url = queue.buildUrl('getArtifact', task, run, 'public/logs/live.log')
-  return requests.get(url, stream=True)
+    auth = tcbuild.read_tc_auth(os.path.expanduser('~/.tcauth'))
+    queue = taskcluster.Queue({'credentials': auth})
+    url = queue.buildUrl('getArtifact', task, run, 'public/logs/live.log')
+    return requests.get(url, stream=True)
+
 
 if __name__ == '__main__':
-  for line in fetch_log(sys.argv[1], 0).iter_lines():
-    print(line)
+    for line in fetch_log(sys.argv[1], 0).iter_lines():
+        print(line)
